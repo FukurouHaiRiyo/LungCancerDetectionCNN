2cb9f7231db3e3d8a0c04e5cff6c126ca41f5b95
FukurouHaiRiyo <andreipanait00@gmail.com>
2023-03-25 16:19:06 +0200

    gui.py updates

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import customtkinter
 from tkinter import filedialog
 from tkinter import *
 from PIL import ImageTk, Image
@@ -17,19 +16,20 @@ classes = {
 
 print(classes)
 
-#initialise GUI
-top=tk.Tk()
+
+# initialise GUI
+top = tk.Tk()
 top.geometry('800x600')
-top.title('CatsVSDogs Classification')
+top.title('Detectarea celuleor anormale la nivelul plamanilor')
 top.configure(background='#CDCDCD')
-label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+label = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
 sign_image = Label(top)
 
 
 def classify(file_path):
     global label_packed
     image = Image.open(file_path)
-    image = image.resize((224,224))
+    image = image.resize((224, 224))
     image = np.expand_dims(image, axis=0)
     image = np.array(image)
     image = image/255
@@ -45,17 +45,17 @@ def classify(file_path):
      
 
 def show_classify_button(file_path):
-    classify_b=Button(top,text="Classify Image", command=lambda: classify(file_path), padx=10, pady=5)
-    classify_b.configure(background='#364156', foreground='white', font=('arial',10,'bold'))
-    classify_b.place(relx=0.79,rely=0.46)
+    classify_b = Button(top, text="Classify Image", command=lambda: classify(file_path), padx=10, pady=5)
+    classify_b.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+    classify_b.place(relx=0.79, rely=0.46)
 
 
 def upload_image():
     try:
-        file_path=filedialog.askopenfilename()
-        uploaded=Image.open(file_path)
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
         uploaded.thumbnail(((top.winfo_width()/2.25), (top.winfo_height()/2.25)))
-        im=ImageTk.PhotoImage(uploaded)
+        im = ImageTk.PhotoImage(uploaded)
         sign_image.configure(image=im)
         sign_image.image=im
         label.configure(text='')
@@ -63,14 +63,15 @@ def upload_image():
     except:
         pass
 
+
 if __name__ == '__main__':
-    upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
-    upload.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
-    upload.pack(side=BOTTOM,pady=50)
-    sign_image.pack(side=BOTTOM,expand=True)
-    label.pack(side=BOTTOM,expand=True)
-    heading = Label(top, text="CatsVSDogs Classification",pady=20, font=('arial',20,'bold'))
-    heading.configure(background='#CDCDCD',foreground='#364156')
+    upload = Button(top, text="Upload an image", command=upload_image, padx=10,  pady=5)
+    upload.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+    upload.pack(side=BOTTOM, pady=50)
+    sign_image.pack(side=BOTTOM, expand=True)
+    label.pack(side=BOTTOM, expand=True)
+    heading = Label(top, text="CatsVSDogs Classification",pady=20, font=('arial', 20, 'bold'))
+    heading.configure(background='#CDCDCD', foreground='#364156')
     heading.pack()
     top.mainloop()
 
